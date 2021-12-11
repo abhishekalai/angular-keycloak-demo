05508b031d73e07aeaf80c9bd08ba2211871f4e1
Abhishek Alai <abbhishek971@gmail.com>
2021-12-11 10:21:35 +0530

    Updated Demo Configurations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-kc';
+
+  constructor(private oauthService: OAuthService) {}
+
+  logout() {
+    this.oauthService.logOut();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthConfigModule } from './auth-config.module';
+import { HomeComponent } from './home/home.component';
+import { LogoutComponent } from './logout/logout.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent },
+      { path: 'logout', component: LogoutComponent },
+    ]),
+    AuthConfigModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
